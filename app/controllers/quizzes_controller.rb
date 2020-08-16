e8598b1dd9365bcc94cb0bc079546437fe4513ac
dukhyunko93 <dukhyunko93@gmail.com>
2020-08-16 19:35:24 -0400

    POST Done

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -11,4 +11,18 @@ class QuizzesController < ApplicationController
         
         render json: quiz, include:[:questions]
     end
+
+    def create
+        quiz = Quiz.create(quiz_params)
+        quizzes = Quiz.all
+        render json: quizzes, include:[:questions]
+    end
+
+    private
+    
+    def quiz_params
+        params.require(:quizzes).permit(:unique_code, :teacher_name, :teacher_email, :title, :time_limit)
+    end
+
 end
+
